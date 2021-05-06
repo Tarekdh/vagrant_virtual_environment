cdf0676363c480484ded89870241b36c091b43bf
Tarek Khachnaoui <t.khachnaoui@oodrive.com>
2021-05-06 14:17:34 +0100

    first commit

---
+++ clone_docker_compose.sh
@@ -0,0 +1,6 @@
+rm /home/vagrant/workspace
+mkdir /home/vagrant/workspace
+cd /home/vagrant/workspace
+git clone https://github.com/Tarekdh/Crud_Mvc_App.git
+cd Crud_Mvc_App/Crud_Mvc_App
+docker-compose up
